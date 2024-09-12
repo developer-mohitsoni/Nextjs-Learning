d7da655dcaedd1abce8a366ed319867ecf3cd6a2
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-12 14:55:40 +0530

    Understand how the redirect method is work

--- src/app/accounts/page.jsx
+++ src/app/accounts/page.jsx
@@ -1,3 +1,12 @@
+import { redirect } from "next/navigation";
+
 export default function Accounts() {
+  // assume that profile info is null
+
+  const userProfileInfo = null;
+
+  if (userProfileInfo === null) {
+    redirect("profile");
+  }
   return <h1>Accounts</h1>;
 }
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <>
+      <div>
+        <h1>This is our Profile Page</h1>
+      </div>
+    </>
+  );
+}
