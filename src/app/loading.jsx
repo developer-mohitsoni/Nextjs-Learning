66f29c81a7bde9f6c5128f716254a71642e22602
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-12 17:12:25 +0530

    Concept of Loading UI

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading(){
+    return (
+        <>
+            <div>
+                <h1 classname="text-5xl">Loading! Please Wait...</h1>
+            </div>
+        </>
+    )
+}
