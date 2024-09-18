8ce6af6371eb883f42fe8d0a3ecfd721aa4dcfd6
Mohit Soni <developer_mohitsoni@outlook.com>
2024-09-18 19:06:30 +0530

    Fetch single user detail in server component

--- src/app/server-data-fetch/[details]/page.jsx
+++ src/app/server-data-fetch/[details]/page.jsx
@@ -1,8 +1,31 @@
-export default function UserDetails() {
+async function fetchSingleUserDetails(currentUserId) {
+  try {
+    const apiResponse = await fetch(
+      `https://dummyjson.com/users/${currentUserId}`
+    );
+
+    const result = await apiResponse.json();
+
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default async function UserDetails({ params }) {
+  //   console.log(params);
+
+  const userDetails = await fetchSingleUserDetails(params.details);
+
   return (
     <>
       <div>
         <h1>This is user detail page</h1>
+        <p>{`Name:- ${userDetails?.firstName} ${userDetails?.lastName}`}</p>
+        <p>{`Age:- ${userDetails?.age}`}</p>
+        <p>{`Email:- ${userDetails?.email}`}</p>
+        <p>{`Phone:- ${userDetails?.phone}`}</p>
+        <p>{`BirthDate:- ${userDetails?.birthDate}`}</p>
       </div>
     </>
   );
