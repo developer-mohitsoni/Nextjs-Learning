4a9e36c4fbc9e9d0933a1f0c3f65c2d90d189286
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-18 18:20:53 +0530

    Fix typo in className attribute in Loading component

--- learning-curve-1/src/app/loading.jsx
+++ learning-curve-1/src/app/loading.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div classname="w-full min-h-screen">
+    <div className="w-full min-h-screen">
       <Skeleton />
     </div>
   );
