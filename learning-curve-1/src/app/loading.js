19223414bf458d7ade4d953202656db5dd66160f
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-18 17:01:59 +0530

    Add Loading component with Skeleton for loading state

---
+++ learning-curve-1/src/app/loading.js
@@ -0,0 +1,9 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div classname="w-full min-h-screen">
+      <Skeleton />
+    </div>
+  );
+}
